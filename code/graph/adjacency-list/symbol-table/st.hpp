7ce1440a9556cace29f6529b4f622895e40d31a7
Mai-icy <Maiicy@Foxmail.com>
2022-04-20 13:31:50 +0800

    修改文件目录

--- code/graph/adjacency-list/symbol-table/st.hpp
+++
@@ -1,1 +0,0 @@
-#include "../base/undirected-graph.hpp"
---
+++ code/graph/undigraph/symbol-table/st.hpp
@@ -0,0 +1,21 @@
+#include "../base/undirected-graph.hpp"
+#include <map>
+#include <string>
+
+class SymbolGraph
+{
+public:
+    SymbolGraph(int size);
+    ~SymbolGraph() { delete str_arr; };
+    bool contains(std::string key) { return str_map.find(key) != str_map.end(); };
+    int index(std::string key) { return str_map[key]; };
+    std::string name(int v) { return str_arr[v]; };
+    Graph G() { return _G; };
+
+private:
+    std::map<std::string, int> str_map;
+    std::string *str_arr;
+    Graph _G;
+};
+
+SymbolGraph::SymbolGraph(int size) : str_arr(new std::string[size]), _G(size) {}
